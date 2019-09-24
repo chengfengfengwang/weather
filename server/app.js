a97e1155c68a9eebff30977529b9dfd9cb16b111
wangchengfeng <602120734@qq.com>
2019-09-24 13:13:46 +0800

    接口

--- server/app.js
+++ server/app.js
@@ -6,8 +6,9 @@ const app = express()
 app.use(express.static('web_src'))
 app.get('/weather', (req, res) => {
     var city = encodeURIComponent(req.query.city);
+    console.log('请求到来');
     axios.get('http://apis.juhe.cn/simpleWeather/query?city='+city+'&key=6fc3096e3e5ee9be2370c793621207a1').then(response=>{
-        res.json(response)
+         res.json(response.data)
     })
     
 })
