070092a8bb925ce68022b1566555cb841e89e9ec
wangchengfeng <602120734@qq.com>
2019-09-24 11:47:49 +0800

    获取天气接口

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const axios = require('axios');
 
-// axios.get('http://apis.juhe.cn/simpleWeather/query?city=%E8%8B%8F%E5%B7%9E&key=6fc3096e3e5ee9be2370c793621207a1').then(res=>{
-//     console.log(res)
-// })
+
 const app = express()
 app.use(express.static('web_src'))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/weather', (req, res) => {
+    var city = encodeURIComponent(req.query.city);
+    axios.get('http://apis.juhe.cn/simpleWeather/query?city='+city+'&key=6fc3096e3e5ee9be2370c793621207a1').then(response=>{
+        res.json(response)
+    })
+    
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
