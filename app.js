ef9b8ccb0e827c70df6fc076fc771c154f4fc216
wcf <602120734@qq.com>
2019-09-30 19:26:02 +0800

    端口修改

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.get('/weather', (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(80, () => console.log('Example app listening on port 80!'))
