a78c3044dc0e4188c4abf782e29b775d64694770
wangchengfeng <602120734@qq.com>
2020-04-28 17:12:34 +0800

    留言

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const koa = require('koa');
 const app = new koa();
+const serve = require('koa-static');
 
 const fs = require('fs');
+const path = require('path');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const requireDirectory = require('require-directory');
+const home   = serve(path.join(__dirname)+'/web_src/');
 
 
 app.use(cors());
 //require('./models/user')
 //异常处理
 app.use(bodyParser());
+app.use(home); 
 //路由
 requireDirectory(module, './api', {
     visit(obj) {
